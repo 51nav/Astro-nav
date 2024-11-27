17886f581dc27a81126bb8842f9af286b0c45de8
51nav <51navpage@gmail.com>
2024-11-27 14:39:50 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://51nav.github.io',
+    integrations: [
+        sitemap({
+            // 自定义配置选项
+            filter: (page) => !page.includes('/submit'), // 排除提交页面
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date(), // 最后修改时间
+        }),
+    ],
 });
