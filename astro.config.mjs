a836d3d507a4d427a93121c654e8f7d3ade24dcd
51nav <51navpage@gmail.com>
2024-11-28 23:40:16 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://51nav.github.io',
+    site: 'https://affnav.github.io',
     integrations: [sitemap()],
 });
