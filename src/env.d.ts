f032c68eba4f0f69b7f96bb9eac4701647856063
sol <sol.ibm@gmail.com>
2025-06-07 21:53:17 +0800

    first commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module 'qrcode' {
+  interface QRCodeOptions {
+    width?: number;
+    margin?: number;
+    color?: {
+      dark?: string;
+      light?: string;
+    };
+  }
+
+  function toCanvas(canvas: HTMLCanvasElement, text: string, options?: QRCodeOptions): Promise<void>;
+
+  export { toCanvas };
+  export default { toCanvas };
+}
